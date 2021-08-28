17d4298843e4b466a71ee8a6e2a6e1a3a4be249b
Murilo Barros Peixoto <38633004+murilombp2214@users.noreply.github.com>
2021-08-28 12:05:21 -0300

    Update URI_2136.cs

--- URI_2136.cs
+++ URI_2136.cs
@@ -14,20 +14,17 @@ namespace URI
         static int maxLengthWord = 0;
         static void Main(string[] args)
         {
-            var friends = new HashSet<KeyValuePair<string, string>>(new DistinctKeyValue());
+            var friends = new HashSet<KeyValuePair<string, string>>();
             Read(friends);
             var newFriends = Order(friends);
             WriteFriends(newFriends);
             Console.WriteLine();
-            WriteWinner(friends, newFriends);
+            WriteWinner(friends);
         }
 
-        private static void WriteWinner(IEnumerable<KeyValuePair<string, string>> friends, IEnumerable<KeyValuePair<string, string>> newFriends)
+        private static void WriteWinner(IEnumerable<KeyValuePair<string, string>> friends)
         {
-            var result = friends.Where(x => x.Value == YES);
-            var person = friends.First(x => x.Value == YES && x.Key.Length == maxLengthWord);
-
-            Console.WriteLine("Amigo do Habay:\n{0}", person.Key);
+            Console.WriteLine("Amigo do Habay:\n{0}", friends.First(x => x.Value == YES && x.Key.Length == maxLengthWord).Key);
         }
 
         private static void WriteFriends(IEnumerable<KeyValuePair<string, string>> newFriends)
@@ -40,17 +37,9 @@ namespace URI
             }
         }
 
-        private static IEnumerable<KeyValuePair<string, string>> Process(HashSet<KeyValuePair<string, string>> friends)
-        {
-            var newFriends = Order(friends);
-            return newFriends;
-        }
-
         private static IEnumerable<KeyValuePair<string, string>> Order(IEnumerable<KeyValuePair<string, string>> friends)
         {
-            return friends.OrderByDescending(x => x.Value)
-                          .ThenBy(x => x.Key);
-
+            return friends.OrderByDescending(x => x.Value).ThenBy(x => x.Key);
         }
 
         private static void Read(HashSet<KeyValuePair<string, string>> friends)
@@ -63,23 +52,5 @@ namespace URI
                     friends.Add(new KeyValuePair<string, string>(keyValue[0], keyValue[1]));
             }
         }
-
-
-
-
-
-        public class DistinctKeyValue : IEqualityComparer<KeyValuePair<string, string>>
-        {
-            public bool Equals(KeyValuePair<string, string> x, KeyValuePair<string, string> y)
-            {
-                return x.Key == y.Key && x.Value == y.Value;
-            }
-
-            public int GetHashCode(KeyValuePair<string, string> obj)
-            {
-                return obj.GetHashCode();
-            }
-        }
-
     }
 }
